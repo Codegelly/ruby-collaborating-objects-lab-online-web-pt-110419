2846830987b3167098940a01f6e53e024d20f77a
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-04 01:45:51 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,15 @@
 class Artist 
-  attr_accessor :name, :songs 
+  attr_accessor :name, :songs, :artist 
   @@all = []
   def initialize(name)
     @name = name 
     @songs = []
+   # @artist = artist
   end 
   
-  def self.all 
+  def self.all
     @@all
+    
   end 
   
   # def songs 
